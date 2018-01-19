b102624544b3c43dcc1e85c63097ce629c9ad663
Nic <nic.ehmayer92@gmail.com>
2018-01-19 00:55:42 +0000

    Added loaded-die exercise

---
+++ loaded-die.js
@@ -0,0 +1,18 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var roll = 0;
+
+
+  return function() {
+    // var currentRoll = list[roll];
+    // roll += 1;
+    //return currentRoll;
+    return list[roll++];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
